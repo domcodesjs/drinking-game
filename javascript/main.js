fffc253d38d954f3eb7faaeb8f54233ebd21745d
Dominic Faulring <domcodesjs@gmail.com>
2020-11-17 11:03:56 -0800

    added scoreboard

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
   const drinkingPerson = document.querySelector('#drinking-person');
   const drinkingButton = document.querySelector('#drinking-button');
-  const drinkers = [
-    'Dominic',
-    'Donovon',
-    'Metty',
-    'Josh',
-    'Ryan',
-    'Angelo',
-    'Noah',
-    'Jason'
+  let drinkers = [
+    { name: 'Dominic', count: 0 },
+    { name: 'Donovon', count: 0 },
+    { name: 'Metty', count: 0 },
+    { name: 'Josh', count: 0 },
+    { name: 'Ryan', count: 0 },
+    { name: 'Angelo', count: 0 },
+    { name: 'Noah', count: 0 },
+    { name: 'Jason', count: 0 }
   ];
   const drinkingArr = new Array(100 - drinkers.length)
     .fill(false)
     .concat(drinkers);
 
+  const renderDrinkers = () => {
+    document.querySelector('#drink-count').innerHTML = drinkers
+      .map((drinker) => `<p><span>${drinker.name}<span> : ${drinker.count}</p>`)
+      .join('');
+  };
+
+  renderDrinkers();
+
   const takeADrink = () => {
     const shuffledArr = shuffle(drinkingArr);
-    const drinker = shuffledArr[Math.floor(Math.random() * shuffledArr.length)];
-    if (!drinker) {
+    const chosenOne =
+      shuffledArr[Math.floor(Math.random() * shuffledArr.length)];
+    if (!chosenOne) {
       return `You got lucky!`;
     }
-    return `${drinker} take a drink!`;
+    const indexOfDrinker = drinkers.findIndex(
+      (drinker) => drinker.name === chosenOne.name
+    );
+    drinkers[indexOfDrinker].count += 1;
+    renderDrinkers();
+    return `${chosenOne.name} take a drink!`;
   };
   const handleDrinker = () => (drinkingPerson.textContent = takeADrink());
   drinkingButton.addEventListener('click', handleDrinker);
