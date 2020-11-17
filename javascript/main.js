b790f362b4af05a51c3085a92230a8494fdf286e
Dominic Faulring <domcodesjs@gmail.com>
2020-11-17 10:49:47 -0800

    moved shuffle helper function outside

--- javascript/main.js
+++ javascript/main.js
@@ -14,33 +14,32 @@ document.addEventListener('DOMContentLoaded', function () {
   const drinkingArr = new Array(100 - drinkers.length)
     .fill(false)
     .concat(drinkers);
-  const shuffle = (array) => {
-    let currentIndex = array.length;
-    let temporaryValue;
-    let randomIndex;
-
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-  };
-  const shuffledArr = shuffle(drinkingArr);
 
   const takeADrink = () => {
+    const shuffledArr = shuffle(drinkingArr);
     const drinker = shuffledArr[Math.floor(Math.random() * shuffledArr.length)];
-    console.log(shuffledArr);
     if (!drinker) {
       return `You got lucky!`;
     }
-
     return `${drinker} take a drink!`;
   };
   const handleDrinker = () => (drinkingPerson.textContent = takeADrink());
   drinkingButton.addEventListener('click', handleDrinker);
 });
+
+function shuffle(array) {
+  let currentIndex = array.length;
+  let temporaryValue;
+  let randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
